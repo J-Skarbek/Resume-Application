ad71eca7658ce0c5c83c2236ada81d68b649fc97
J-Skarbek <john@snarkmediaagency.com>
2023-03-04 11:21:07 -0600

    Build the header and add background animated gradient

---
+++ resume-application/src/components/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react';
+import '../styles/App.css';
+import Header from './Header';
+
+function App() {
+
+  return (
+    <div className="app-body">
+      <Header />
+
+    </div>
+  )
+}
+
+export default App
---
+++ resume-application/src/components/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import '../styles/Header.css';
+import icon from '../assets/resume-cv-icon-white.svg';
+
+function Header() {
+
+  return (
+    <div className="header-bar bg-white/25 flex justify-center py-6 shadow-md">
+      <img src={icon} alt="" className="logo h-20 pr-6" />
+      <div className="header-text">
+        <h1 className="site-name text-white">Build-A-Resume</h1>
+        <p className="site-tagline text-white ">A free, interactive resume-building app.</p>
+      </div>
+    </div>
+  )
+}
+
+export default Header
