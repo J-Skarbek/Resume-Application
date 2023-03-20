9cb6ab6a1d7819cbd9081a83e3f2da246f4f701c
J-Skarbek <john@snarkmediaagency.com>
2023-03-20 17:47:47 -0500

    Dup the header code into the footer for basic footer and make a few CSS changes

--- resume-application/src/components/App.jsx
+++ resume-application/src/components/App.jsx
@@ -1,7 +1,7 @@
-import { useState } from 'react';
 import '../styles/App.css';
 import Header from './Header';
 import Body from './Body';
+import Footer from './Footer';
 
 function App() {
 
@@ -9,6 +9,7 @@ function App() {
     <div className="app-bg">
       <Header />
       <Body />
+      <Footer />
     </div>
   )
 }
