50cb97268633ac51950f13dc02609756f23f4c96
J-Skarbek <john@snarkmediaagency.com>
2023-03-21 14:11:23 -0500

    Flesh out footer and mobile optimize it

--- resume-application/src/components/Footer.jsx
+++ resume-application/src/components/Footer.jsx
@@ -4,11 +4,18 @@ import icon from '../assets/resume-cv-icon-white.svg';
 function Footer() {
 
   return (
-    <div className="header-bar bg-white/25 flex justify-center py-6 shadow-md">
-      <img src={icon} alt="" className="logo h-20 pr-6" />
-      <div className="header-text">
-        <h1 className="site-name text-white">Build-A-Resume</h1>
-        <p className="site-tagline text-white ">A free, interactive resume-building app.</p>
+    <div className="footer bg-white/25 flex flex-col sm:flex-row items-center justify-between py-6 px-12 shadow-md">
+      <div className="footer-left flex flex-col sm:flex-row content-center">
+        <img src={icon} alt="" className="logo h-16 mb-4 sm:mr-6" />
+        <div className="text flex-col text-white text-center sm:text-left">
+          <h2 className="site-name text-3xl font-bold">Build-A-Resume</h2>
+          <p className="site-tagline">A free, interactive resume-building app.</p>
+        </div>
+      </div>
+      <div className="footer-right text-white flex-col text-center sm:text-left">
+        <h2 className="text-xl font-semibold">Created By: John Skarbek</h2>
+        <p>March, 2023</p>
+        <a href="#">View on Github Pages</a>
       </div>
     </div>
   );
