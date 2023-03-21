d8bf8a1d5fe32cbdfad8a703ed4845ab98d25026
J-Skarbek <john@snarkmediaagency.com>
2023-03-21 14:38:59 -0500

    Start mobile optimizing the body elements

--- resume-application/src/components/Body.jsx
+++ resume-application/src/components/Body.jsx
@@ -104,8 +104,8 @@ function Body() {
   // console.log(`Work: ${workExp}`)
 
   return (
-    <div className="body flex justify-evenly my-8 h-screen">
-      <div className="left-side overflow-y-auto">
+    <div className="body flex flex-col sm:flex-row justify-evenly my-8 px-12 h-screen">
+      <div className="left-side w-1/2 overflow-y-auto px-8">
         <h2 className="font-bold text-white text-3xl mb-4">
           Personal Information
         </h2>
@@ -139,7 +139,7 @@ function Body() {
           onChange={handleDayChange}
         /> */}
       </div>
-      <div className="right-side w-[60%]">
+      <div className="right-side px-8 w-1/2">
         <ResumeDisplay
           generalInfoData={generalInfo}
           workExp={workExp}
