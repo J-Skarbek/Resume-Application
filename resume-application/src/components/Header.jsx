27a0eeb55065b6381f5802d3fce8c62738ef25bd
J-Skarbek <john@snarkmediaagency.com>
2023-03-21 14:15:31 -0500

    Add mobile responsiveness to the header

--- resume-application/src/components/Header.jsx
+++ resume-application/src/components/Header.jsx
@@ -5,11 +5,11 @@ import icon from '../assets/resume-cv-icon-white.svg';
 function Header() {
 
   return (
-    <div className="header-bar bg-white/25 flex justify-center py-6 shadow-md">
+    <div className="header-bar bg-white/25 flex flex-col sm:flex-row justify-center py-6 shadow-md">
       <img src={icon} alt="" className="logo h-20 pr-6" />
-      <div className="header-text">
-        <h1 className="site-name text-white">Build-A-Resume</h1>
-        <p className="site-tagline text-white ">A free, interactive resume-building app.</p>
+      <div className="header-text text-white text-center sm:text-left">
+        <h1 className="site-name text-3xl sm:text-5xl">Build-A-Resume</h1>
+        <p className="site-tagline">A free, interactive resume-building app.</p>
       </div>
     </div>
   )
